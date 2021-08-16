44f23aa8e94b236a501a61f3e5ebd130b25ea85e
jaemoon <jaemoon@jaemoons-MacBook-Pro.local>
2021-08-16 21:41:05 +0900

    Updates

---
+++ precache-manifest.5049f1016dc2796ebb65115720c7c6f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e9193585b8443652b900fbf2b4be95",
+    "url": "/nami-frontend/index.html"
+  },
+  {
+    "revision": "9920347103e314601eb1",
+    "url": "/nami-frontend/static/css/2.cd1c2086.chunk.css"
+  },
+  {
+    "revision": "b0869719b3f78ffec57d",
+    "url": "/nami-frontend/static/css/main.b8be3489.chunk.css"
+  },
+  {
+    "revision": "9920347103e314601eb1",
+    "url": "/nami-frontend/static/js/2.da7595f2.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/nami-frontend/static/js/2.da7595f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b0869719b3f78ffec57d",
+    "url": "/nami-frontend/static/js/main.85aa368b.chunk.js"
+  },
+  {
+    "revision": "53b2f8975a565ea68959",
+    "url": "/nami-frontend/static/js/runtime-main.90d0c5d1.js"
+  }
+]);
